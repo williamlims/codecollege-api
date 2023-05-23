41b5f70353f7f705c19696f13d5a5a1b0da7498c
William Lima <williamlims@hotmail.com>
2023-05-23 00:20:24 -0300

    implement ssl for https response

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const app = require("./app");
+const https = require("https");
+const fs = require("fs");
 
 const port = 3001;
 
-app.listen(port, () => {
+const certs = {
+    key: fs.readFileSync("src/services/cert/key.pem"),
+    cert: fs.readFileSync("src/services/cert/cert.pem"),
+};
+
+https.createServer(certs, app).listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 });
