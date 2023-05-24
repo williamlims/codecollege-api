f9ff1b333353a1000d04dd810400a9c01238cfab
William Lima <williamlims@hotmail.com>
2023-05-24 18:53:10 -0300

    Update UserSetController.js
    changing the table names

--- src/controllers/UserSetController.js
+++ src/controllers/UserSetController.js
@@ -16,84 +16,84 @@ routes.get('/courses/:id', async (req, res) => {
 });
 
 routes.get('/courses/:id/classes', async (req, res) => {
-    const sql = "select courses.idControl, courses.nameCourse, courses.description, "+
-                "classes.idControl as idClass, classes.nameClass, classes.moduleControl, classes.moduleOrder "+
-                "FROM classes INNER JOIN courses "+
-                "ON classes.idCourseControl = courses.idControl "+
-                "where courses.idControl = (:id) "+
-                "order by classes.moduleControl, classes.moduleOrder asc";
+    const sql = "select Courses.idControl, Courses.nameCourse, Courses.description, "+
+                "Classes.idControl as idClass, Classes.nameClass, Classes.moduleControl, Classes.moduleOrder "+
+                "FROM Classes INNER JOIN Courses "+
+                "ON Classes.idCourseControl = Courses.idControl "+
+                "where Courses.idControl = (:id) "+
+                "order by Classes.moduleControl, Classes.moduleOrder asc";
     const classes = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(classes);
 });
 
 routes.get('/courses/:idCourse/classes/:idClass', async (req, res) => {
-    const sql = "select courses.idControl, courses.nameCourse, courses.description, "+
-                "classes.idControl as idClass, classes.nameClass, classes.moduleControl, classes.moduleOrder, classes.linkYoutube "+
-                "FROM classes INNER JOIN courses "+
-                "ON classes.idCourseControl = courses.idControl "+
-                "where courses.idControl = (:idCourse) and classes.idControl = (:idClass) "
+    const sql = "select Courses.idControl, Courses.nameCourse, Courses.description, "+
+                "Classes.idControl as idClass, Classes.nameClass, Classes.moduleControl, Classes.moduleOrder, Classes.linkYoutube "+
+                "FROM Classes INNER JOIN Courses "+
+                "ON Classes.idCourseControl = Courses.idControl "+
+                "where Courses.idControl = (:idCourse) and Classes.idControl = (:idClass) "
     const classs = await db.sequelize.query(sql, { replacements: {idCourse: req.params.idCourse, idClass: req.params.idClass}, type: db.sequelize.QueryTypes.SELECT});
     res.json(classs);
 });
 
 routes.get('/tutorials/user/:id', async (req, res) => {
-    const sql = "SELECT tutorials.idControl, tutorials.nameTutorial, tutorials.filePath "+
-                "FROM tutorials INNER JOIN grouptutorials "+
-                "ON tutorials.idControl = grouptutorials.idControlTutorial INNER JOIN codecollege.groups "+
-                "ON grouptutorials.idControlGroup = codecollege.groups.idControl INNER JOIN groupusers "+
-                "ON codecollege.groups.idControl = groupusers.idControlGroup INNER JOIN users "+
-                "ON groupusers.idControlUser = users.idControl "+
-                "where users.idControl = (:id) ";
+    const sql = "SELECT Tutorials.idControl, Tutorials.nameTutorial, Tutorials.filePath "+
+                "FROM Tutorials INNER JOIN GroupTutorials "+
+                "ON Tutorials.idControl = GroupTutorials.idControlTutorial INNER JOIN codecollege.Groups "+
+                "ON GroupTutorials.idControlGroup = codecollege.Groups.idControl INNER JOIN GroupUsers "+
+                "ON codecollege.Groups.idControl = GroupUsers.idControlGroup INNER JOIN Users "+
+                "ON GroupUsers.idControlUser = Users.idControl "+
+                "where Users.idControl = (:id) ";
     const tutorials = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(tutorials);
 });
 
 routes.get('/classes/user/:id', async (req, res) => {
-    const sql = "SELECT freeclasses.idControl, freeclasses.nameClass, freeclasses.description "+
-                "FROM freeclasses INNER JOIN groupclasses "+
-                "ON freeclasses.idControl = groupclasses.idControlClass INNER JOIN codecollege.groups "+
-                "ON groupclasses.idControlGroup = codecollege.groups.idControl INNER JOIN groupusers "+
-                "ON codecollege.groups.idControl = groupusers.idControlGroup INNER JOIN users "+
-                "ON groupusers.idControlUser = users.idControl "+
-                "where users.idControl = (:id) ";
+    const sql = "SELECT FreeClasses.idControl, FreeClasses.nameClass, FreeClasses.description "+
+                "FROM FreeClasses INNER JOIN GroupClasses "+
+                "ON FreeClasses.idControl = GroupClasses.idControlClass INNER JOIN codecollege.Groups "+
+                "ON GroupClasses.idControlGroup = codecollege.Groups.idControl INNER JOIN GroupUsers "+
+                "ON codecollege.Groups.idControl = GroupUsers.idControlGroup INNER JOIN Users "+
+                "ON GroupUsers.idControlUser = Users.idControl "+
+                "where Users.idControl = (:id) ";
     const classes = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(classes);
 });
 
 routes.get('/classes/class/:id', async (req, res) => {
-    const sql = "select * from freeclasses where idControl = (:id)";
+    const sql = "select * from FreeClasses where idControl = (:id)";
     const classes = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(classes);
 });
 
 routes.get('/libraries/user/:id', async (req, res) => {
-    const sql = "SELECT libraries.idControl, libraries.nameDocument, libraries.filePath "+
-                "FROM libraries INNER JOIN grouplibraries "+
-                "ON libraries.idControl = grouplibraries.idControlLibrary INNER JOIN codecollege.groups "+
-                "ON grouplibraries.idControlGroup = codecollege.groups.idControl INNER JOIN groupusers "+
-                "ON codecollege.groups.idControl = groupusers.idControlGroup INNER JOIN users "+
-                "ON groupusers.idControlUser = users.idControl "+
-                "where users.idControl = (:id) ";
+    const sql = "SELECT Libraries.idControl, Libraries.nameDocument, Libraries.filePath "+
+                "FROM Libraries INNER JOIN GroupLibraries "+
+                "ON Libraries.idControl = GroupLibraries.idControlLibrary INNER JOIN codecollege.Groups "+
+                "ON GroupLibraries.idControlGroup = codecollege.Groups.idControl INNER JOIN GroupUsers "+
+                "ON codecollege.Groups.idControl = GroupUsers.idControlGroup INNER JOIN Users "+
+                "ON GroupUsers.idControlUser = Users.idControl "+
+                "where Users.idControl = (:id) ";
     const documents = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(documents);
 });
 
 routes.get('/certificates/user/:idu/:idc', async (req, res) => {
-    const sql = "SELECT users.firstName, users.lastName, courses.nameCourse, certificates.conclusionDate "+
-                "FROM courses INNER JOIN certificates "+
-                "ON courses.idControl = certificates.idCourseControl INNER JOIN users "+
-                "ON certificates.idUserControl = users.idControl "+
-                "where users.idControl = (:idu) and courses.idControl = (:idc)  ";
+    const sql = "SELECT Users.firstName, Users.lastName, Courses.nameCourse, Certificates.conclusionDate "+
+                "FROM Courses INNER JOIN Certificates "+
+                "ON Courses.idControl = Certificates.idCourseControl INNER JOIN Users "+
+                "ON Certificates.idUserControl = Users.idControl "+
+                "where Users.idControl = (:idu) and Courses.idControl = (:idc)  ";
     const certificates = await db.sequelize.query(sql, { replacements: {idu: req.params.idu, idc: req.params.idc}, type: db.sequelize.QueryTypes.SELECT});
     res.json(certificates);
 });
 
 routes.get('/certificates/user/:id', async (req, res) => {
-    const sql = "SELECT users.firstName, users.lastName, courses.nameCourse, certificates.conclusionDate, courses.idControl "+
-                "FROM courses INNER JOIN certificates "+
-                "ON courses.idControl = certificates.idCourseControl INNER JOIN users "+
-                "ON certificates.idUserControl = users.idControl "+
-                "where users.idControl = (:id) ";
+    const sql = "SELECT Users.firstName, Users.lastName, Courses.nameCourse, Certificates.conclusionDate, Courses.idControl "+
+                "FROM Courses INNER JOIN Certificates "+
+                "ON Courses.idControl = Certificates.idCourseControl INNER JOIN Users "+
+                "ON Certificates.idUserControl = Users.idControl "+
+                "where Users.idControl = (:id) ";
     const certificates = await db.sequelize.query(sql, { replacements: {id: req.params.id}, type: db.sequelize.QueryTypes.SELECT});
     res.json(certificates);
 });
